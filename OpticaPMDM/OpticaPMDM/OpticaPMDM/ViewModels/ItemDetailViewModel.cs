4c4dc808b7225ad412c61a6809726df834729b8e
Jorge Solanas Jiménez <jorge.solanas@gmail.com>
2020-05-02 09:47:29 +0200

    Inicio

---
+++ OpticaPMDM/OpticaPMDM/OpticaPMDM/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using OpticaPMDM.Models;
+
+namespace OpticaPMDM.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
